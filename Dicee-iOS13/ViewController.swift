10b93245bacc42cbe8056e3100ccbf8c8d90fbd3
Ryan Leach <rwmleach@gmail.com>
2020-03-06 13:21:18 -0500

    pushing button changes random image

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var diceImageView1: UIImageView!
+	@IBOutlet weak var diceImageView2: UIImageView!
+	
+	let diceImages = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+		roll()
     }
 
-
+	@IBAction func rollButtonPressed(_ sender: UIButton) {
+		roll()
+	}
+	
+	func roll() {
+		diceImageView1.image = diceImages.randomElement()
+		diceImageView2.image = diceImages.randomElement()
+	}
 }
-
